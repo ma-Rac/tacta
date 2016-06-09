9ddfa6c1daed48bc542e3eee5960eaebfcc85820
ma-Rac <mno.racca@gmail.com>
2016-06-09 05:40:24 -0300

    new create method and a few things.

--- tacta.rb
+++ tacta.rb
@@ -29,13 +29,39 @@ contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@the
 
 
 loop do
-  index(contacts)
+   index( contacts )
 
-  response = ask("Who would you like to see? (q to quit):\n")
-  break if response.downcase == 'q'
-  i = response.to_i
+   puts
+   response = ask "Who would you like to see (n for new, q to quit)? "
 
-  contact = contacts[i-1]
+   break if response.downcase == 'q'
 
-  show(contact)
+   if response.downcase == 'n'
+      contact = {}
+
+      puts
+      puts "Enter contact info:"
+
+      contact[:name ] = ask "Name? "
+      contact[:phone] = ask "Phone? "
+      contact[:email] = ask "Email? "
+
+      contacts << contact
+
+      puts
+      puts "New contact created:"
+      puts
+
+      show( contact )
+      puts
+   else
+      i = response.to_i
+
+      contact = contacts[i-1]
+
+      puts
+      show( contact )
+      puts
+   end
+
 end
